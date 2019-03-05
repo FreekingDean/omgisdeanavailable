032ca4d77c85f5956c340ad1ba817e980b89f677
Dean <deangalvin3@gmail.com>
2019-03-05 10:50:40 -0500

    init

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var page = `<html lang="en"><head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--  -->
+		<meta property="og:title" content="Is it Wednesday yet?">
+		<meta property="og:description" content="The question you've been asking yourself.">
+		<meta property="og:url" content="http://www.isitwednesdayyet.com/">
+		<!-- -->
+		<title>OMG Is Dean Available Right Now?</title>
+		<link href="https://fonts.googleapis.com/css?family=Lato:900,300" rel="stylesheet" type="text/css">
+		<style>
+			body {
+				font-family: 'Lato', sans-serif;
+				text-shadow: 2px 2px #222;
+				color: #EBEBEB;
+				background-color: #15171A;
+				background-repeat: repeat;
+			}
+
+			#main-div {
+				margin-top: 5em;
+			}
+
+			.center {
+				margin-left: auto;
+				margin-right: auto;
+				width: 15em;
+				text-align: center;
+			}
+
+			#question {
+				font-size: 20pt;
+				font-weight: 300;
+			}
+
+			#answer {
+				margin-top: 3em;
+				font-size: 50pt;
+				font-weight: 900;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="main-div">
+			<div id="question" class="center">Is it Wednesday yet?</div>
+			<div id="answer" class="center">No :( It's only Tuesday ..</div>
+		</div>
+	</body>
+</html>`
+
+func main() {
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, page)
+	})
+	http.ListenAndServe(":"+port, nil)
+}
